86a261651d287abb9415c136997308c84abe6aae
Mikael Wilhelmsson <micke@nevyn.se>
2022-09-01 10:29:02 +0200

    Update configuration.nix

--- system/configuration.nix
+++ system/configuration.nix
@@ -23,9 +23,7 @@
     };
   };
 
-  boot.kernelModules = [ "i2c-dev" "i2c-piix4" "kvm-amd" ];
-  boot.initrd.kernelModules = [ "amdgpu" ];
-
+  boot.kernelModules = [ "i2c-dev" "i2c-piix4" ];
 
   nix = {
     package = pkgs.nixFlakes;
@@ -40,15 +38,9 @@
   # Enables wireless support via wpa_supplicant.
   networking.networkmanager.enable = true;
 
-  # The global useDHCP flag is deprecated, therefore explicitly set to false here.
-  # Per-interface useDHCP will be mandatory in the future, so this generated config
-  # replicates the default behaviour.
-  networking.useDHCP = false;
-  networking.interfaces.enp6s0.useDHCP = true;
-  networking.interfaces.wlp5s0.useDHCP = true;
 
   i18n.defaultLocale = "en_US.UTF-8";
-  time.timeZone = "Asia/Kolkata";
+  time.timeZone = "Europe/Stockholm";
 
   # List packages installed in system profile. To search, run:
   # $ nix search wget
@@ -64,11 +56,7 @@
   programs.dconf.enable = true;
 
   # List services that you want to enable:
-  services.openssh.enable = true;
-
-  # Logitech wireless device setup
-  hardware.logitech.wireless.enable = true;
-  hardware.logitech.wireless.enableGraphical = true; # for solaar to be included
+  services.openssh.enable = false;
 
   # Enable sound.
   sound.enable = true;
@@ -86,13 +74,8 @@
   services.xserver = {
     enable = true;
     xkbOptions = "caps:swapescape";
-    videoDrivers = [ "amdgpu" ];
     autoRepeatDelay = 200;
     autoRepeatInterval = 20;
-
-    displayManager.setupCommands = ''
-      ${pkgs.xorg.xrandr}/bin/xrandr --output DisplayPort-0 --mode 3840x2160 --scale 0.70x0.70 --output DisplayPort-1 --mode 2560x1440 --rotate left --left-of DisplayPort-0
-    '';
 
     desktopManager.session = [
       {
@@ -106,7 +89,7 @@
   };
 
   # Define a user account. Don't forget to set a password with ‘passwd’.
-  users.users.sherub = {
+  users.users.micke = {
     isNormalUser = true;
     # Enable ‘sudo’, 'audio' for the user.
     extraGroups = [ "wheel" "audio" "docker" "qemu-libvirtd" "libvirtd" ];
@@ -126,7 +109,7 @@
   # (e.g. man configuration.nix or on https://nixos.org/nixos/options.html).
   system.stateVersion = "20.09"; # Did you read the comment?
 
-  users.extraGroups.vboxusers.members = [ "sherub" ];
+  users.extraGroups.vboxusers.members = [ "micke" ];
   virtualisation.virtualbox.host.enable = true;
   virtualisation.libvirtd.enable = true;
   virtualisation.docker.enable = true;
